b3174598f84780e894064296b8956028d42fff76
csg <imgokulcs@gmail.com>
2025-12-16 09:17:59 +0000

    Register models to Admin

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ProcedureType
 
-# Register your models here.
+
+admin.site.register(ProcedureType)
--- apps/clinical/admin.py
+++ apps/clinical/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import Patient, Clinician, Department
+
+admin.site.register(Patient)
+admin.site.register(Clinician)
+admin.site.register(Department)
